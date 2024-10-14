216736ccf044aaeb1d0f50fdddb9a917a432e2f5
ndlov <ndlov@10.76.232.156>
2024-10-14 09:35:38 +0200

    First

--- src/progsitin/PROGsitin.java
+++ src/progsitin/PROGsitin.java
@@ -15,6 +15,9 @@ public class PROGsitin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Start app
+        
+        System.out.println("f");
         
     }
     
