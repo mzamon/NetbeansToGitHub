90725b9915be401e31dbbc58f47e73b75c6fb38b
ndlov <ndlov@10.76.232.156>
2024-10-14 09:59:08 +0200

    Second

--- src/progsitin/PROGsitin.java
+++ src/progsitin/PROGsitin.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package progsitin;
-
+import java.util.Scanner;
 /**
  *
  * @author ndlov
@@ -16,8 +16,22 @@ public class PROGsitin {
     public static void main(String[] args) {
         // TODO code application logic here
         //Start app
+        //Initialize Scanner
+        Scanner scanner = new Scanner(System.in);
+        String [][] CityRoads = new String[3][3]; // Initialize a two dimensional array
+        String [] MostRoads = new String[3]; //Initialize a one dimensional array
         
-        System.out.println("f");
+        for (int k = 1; CityRoads.length; k++ ){
+            System.out.println("Enter the number of roads for Cape Town :");
+            
+            
+        }
+        
+        
+        
+        System.out.println("Enter the number of roads for Johannesburg :");
+        System.out.println("Enter the number of roads for Port Elizabeth :");
+        
         
     }
     
